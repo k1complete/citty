df8a3adfd42aa0790612336dfe2406d69f64a6ad
k1complete <oda@maqcsa.net>
2014-05-06 18:32:32 +0900

    add -f len

--- src/citty.c
+++ src/citty.c
@@ -11,15 +11,64 @@
 #include "config.h"
 static int sigchilded = 0;
 
-int loop(int master, regex_t *preg) 
+int unescape_meta(char *src, char **dest, int len) 
+{
+        int i;
+        *dest = malloc(strlen(src) * 2 + 1 + len);
+        for (i = 0; src[i]; i++) {
+                (*dest)[i] = '\\';
+                (*dest)[i+1] = src[i];
+        }
+        return 0;
+}
+
+int regrecomp_stdin_nchar(regex_t *preg, int feedn)
+{
+        char *pbuf = NULL;
+        size_t pbuflen = feedn + 1;
+        int readed = 0;
+        char *pattern = NULL;
+        int regerr;
+        char errbuf[BUFSIZ];
+        char fmtbuf[BUFSIZ];
+
+        if ((pbuf = malloc(pbuflen)) == NULL) {
+                perror("malloc");
+                return -1;
+        }
+        memset(pbuf, 0, pbuflen);
+        snprintf(fmtbuf, sizeof(fmtbuf), "%%%ds ", feedn);
+        fscanf(stdin, fmtbuf, pbuf);
+        /*        
+                if ((readed != fread(pbuf, 1, feedn, stdin)) == feedn) {
+                perror("fread");
+                return -1;
+                }
+        */
+        regfree(preg);
+        unescape_meta(pbuf, &pattern, 2);
+        free(pbuf);
+        strcat(pattern, " $");
+        /*        printf("prompt regex [%s]\n", pbuf);  */
+        if ((regerr = regcomp(preg, pattern, 0))) {
+                regerror(regerr, preg, errbuf, sizeof(errbuf));
+                return -1;
+        }
+        free(pattern);
+        return 0;
+}
+int loop(int master, regex_t *preg, int feedn) 
 {
         char buf[1000];
         char inbuf[1000];
+        int buflen;
+        char *promptbuf = NULL;
         fd_set fdrs;
         fd_set fdws;
         fd_set fdes;
         int prompt = 0;
-        int buflen = 0;
+        int change_prompt = 1;
+
         while (1) {
                 memset(buf, 0, sizeof(buf));
                 memset(inbuf, 0, sizeof(inbuf));
@@ -47,11 +96,19 @@ int loop(int master, regex_t *preg)
                                 if (fgets(inbuf, sizeof(inbuf), stdin)) {
                                         write(master, inbuf, strlen(inbuf));
                                         prompt = 0;
+                                        change_prompt = 1;
                                 } else {
                                         return -1;
                                 }
                         }
                 } else {
+                        if (change_prompt && 
+                            FD_ISSET(STDIN_FILENO, &fdrs) && feedn > 0) {
+                                if (regrecomp_stdin_nchar(preg, feedn)) {
+                                        return -1;
+                                }
+                                change_prompt = 0;
+                        }
                         if (FD_ISSET(master, &fdrs) || 
                             FD_ISSET(master, &fdes)) {
                                 if ((buflen = read(master, buf, sizeof(buf))) < 0) {
@@ -71,6 +128,7 @@ int usage(const char *desc)
 {
         printf("usage: %s %s\n", PACKAGE_NAME, desc);
         printf("-p regexp : prompt regexp(default '\\$ $' for bash)\n");
+        printf("-f len : len chars ahead each line for prompt )\n");
         return 0;
 }
 void sigchild(int no)
@@ -82,8 +140,8 @@ int main(int argc, char * const argv[], char * const env[])
         int master;
         pid_t pid;
         int longindex = 0;
-        char *optstring = "+p:";
-        char *optdef = "-p prompt command [args...]";
+        char *optstring = "+p:f:";
+        char *optdef = "[-p prompt | -f n]command [args...]";
         regex_t preg;
         int r = 0;
         char *const *av;
@@ -91,6 +149,7 @@ int main(int argc, char * const argv[], char * const env[])
         char *prompt = "\\$ $";
         int regerr;
         char errbuf[BUFSIZ];
+        int feedn = 0;
         struct sigaction  sigact;
         memset(&sigact, 0, sizeof(sigact));
         sigact.sa_handler = sigchild;
@@ -102,6 +161,10 @@ int main(int argc, char * const argv[], char * const env[])
                 switch (r) {
                 case 'p':
                         cust_prompt = strdup(optarg);
+                        break;
+                case 'f':
+                        feedn = atoi(optarg);
+                        printf("aaa %d\n", feedn);
                         break;
                 default:
                         printf("%d\n", r);
@@ -133,7 +196,7 @@ int main(int argc, char * const argv[], char * const env[])
                         exit(1);
                 }
         }
-        loop(master, &preg);
+        loop(master, &preg, feedn);
         regfree(&preg);
         kill(pid, SIGTERM);
         close(master);
